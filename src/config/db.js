e274db69777b66dcab7c613c09206e73b5295725
furahaderick <yvesderricks@gmail.com>
2025-04-09 15:03:21 +0200

    mongo

---
+++ src/config/db.js
@@ -0,0 +1,29 @@
+import chalk from "chalk";
+import mongoose from "mongoose";
+import ora from "ora";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+export const startDB = async () => {
+	try {
+		const spinner = ora("Connecting to mongoDB...").start();
+		await mongoose.connect(process.env.MONGO_URI);
+		spinner.stop();
+		console.log(chalk.greenBright("Successfully connected to mongoDB."));
+	} catch (err) {
+		console.log(chalk.redBright("\nCouldn't connect to mongoDB. Try again."));
+		process.exit(1);
+	}
+};
+
+export const stopDB = async () => {
+	try {
+		await mongoose.disconnect();
+		console.log(chalk.greenBright("Successfully disconnected from mongoDB."));
+	} catch (err) {
+		console.log(
+			chalk.redBright("\nError disconnecting from mongoDB. Try again.")
+		);
+	}
+};
