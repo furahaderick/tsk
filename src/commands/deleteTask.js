44eb0cabe38b1d3604f2b5e37911031db1e86508
furahaderick <yvesderricks@gmail.com>
2025-04-12 15:39:18 +0200

    Other CRUD operations

--- src/commands/deleteTask.js
+++ src/commands/deleteTask.js
@@ -1,3 +1,23 @@
+import { input } from "@inquirer/prompts";
+import ora from "ora";
+import chalk from "chalk";
+
+import Task from "../models/task.model.js";
+
 export const deleteTask = async () => {
-	console.log("Deleted task");
+	const descriptor = await input({ message: "Enter the task descriptor: " });
+	const spinner = ora("Fetching task...").start();
+	const task = await Task.findOne({ descriptor });
+	spinner.stop();
+
+	if (!task) {
+		console.log(
+			chalk.redBright(`Couldn't find task with descriptor '${descriptor}'`)
+		);
+	} else {
+		spinner.start("Deleting task...");
+		await Task.findOneAndDelete({ descriptor });
+		spinner.stop();
+		console.log(chalk.greenBright("Task deleted successfully"));
+	}
 };
--- src/commands/updateTask.js
+++ src/commands/updateTask.js
@@ -1,3 +1,31 @@
+import { input } from "@inquirer/prompts";
+import ora from "ora";
+import chalk from "chalk";
+
+import Task from "../models/task.model.js";
+
 export const updateTask = async () => {
-	console.log("Updated task");
+	const descriptor = await input({ message: "Enter the task descriptor: " });
+
+	const spinner = ora("Fetching task...").start();
+	const task = await Task.findOne({ descriptor });
+	spinner.stop();
+
+	if (!task) {
+		console.log(
+			chalk.redBright(`Couldn't find task with descriptor '${descriptor}'`)
+		);
+	} else {
+		const newTask = {
+			title: await input({ message: "Enter new task title: " }),
+			description: await input({ message: "Enter new task description: " }),
+		};
+
+		spinner.start("Updating task...");
+
+		await Task.findOneAndUpdate({ descriptor }, newTask);
+
+		spinner.stop();
+		console.log(chalk.greenBright("Task updated successfully"));
+	}
 };
